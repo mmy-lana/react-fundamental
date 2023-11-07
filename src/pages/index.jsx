a86021107950dd53a8027d5d2f7c0cadc82abc85
mmy-lana <mmaulanayusuf777@gmail.com>
2023-11-07 11:35:02 +0700

    props and child component, basic blog with JSON

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import Article from "../components/Article";
+import posts from "../posts.json";
+function Homepage() {
+  return (
+    <>
+      <h1>Simple Blog</h1>
+      {posts.map((blog) => (
+        <Article title={blog.title} tags={blog.tags} date={blog.date} />
+      ))}
+    </>
+  );
+}
+
+export default Homepage;

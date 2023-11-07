4906521f9448c6fb2bd1d231385b2cd0f9383ee2
mmy-lana <mmaulanayusuf777@gmail.com>
2023-11-07 13:08:08 +0700

    return value to parent using props, conditional rendering

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,9 +1,19 @@
+const ArticleStatus = ({ isNew }) => {
+  return isNew && <span>-- New !!</span>;
+};
+
+// const NewArticle = () => {
+//   return <span>-- NEW !! </span>;
+// };
+
 function Article(props) {
   return (
     <>
       <h3>{props.title}</h3>
       <small>
-        Date : {props.date}, tags: {props.tags.join(", ")}
+        Date : {props.date}, tags: {props.tags.join(", ")}{" "}
+        <ArticleStatus isNew={props.isNew} />
+        {/* {props.isNew && <NewArticle />} */}
       </small>
     </>
   );
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import Article from "../components/Article";
-import posts from "../posts.json";
+import postsData from "../posts.json";
+import Search from "../components/Search";
+
 function Homepage() {
+  const [posts, setPosts] = useState(postsData);
+  const [totalPosts, setTotalPosts] = useState(0);
+  const onSearchChange = (value) => {
+    console.log(value);
+    const filteredPosts = postsData.filter((item) =>
+      item.title.includes(value)
+    );
+    setPosts(filteredPosts);
+    setTotalPosts(filteredPosts.length);
+  };
   return (
     <>
       <h1>Simple Blog</h1>
-      {posts.map((blog) => (
-        <Article title={blog.title} tags={blog.tags} date={blog.date} />
+      <Search onSearchChange={onSearchChange} totalPosts={totalPosts} />
+      {posts.map((props, index) => (
+        <Article {...props} key={index} />
       ))}
     </>
   );
